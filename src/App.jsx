f83d1e7bdf89ce9d778e57cc3fcf4e592372dba4
Nigel Mupira <h230828v@hit.ac.zw>
2024-09-05 22:43:46 +0200

    Inline Styles
    Example of inline styling in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <div className='text-5xl'>
         App
       </div>
-      <p>Hello {name}</p>
+      <p style={{color:'purple', fontSize:'24px'}}>Hello {name}</p>
       <p>The sume of {x} and {y} is {x + y}</p>
       <ul>
         {names.map((name, index) => (
