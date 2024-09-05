683a652536a3963f4eb71e22eac35c759a7452a7
Nigel Mupira <h230828v@hit.ac.zw>
2024-09-05 22:52:18 +0200

    Clearing the board
    That conclude the short JSX crash course (from creating a class using tailwind till now)

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,8 @@
 import React from 'react'
 
-function App() {
-  const name = 'Nigel';
-  const x = 3;
-  const y = 7;
-  const names = ['Mboks', 'Witiza', 'Mphathisi', 'Maposa']
-  const loggedIn = true;
-  const styles = {
-    color: 'purple',
-    fontSize: '24px'
-  };
-
+const App = () => {
   return (
-    <div>
-      <div className='text-5xl'>
-        App
-      </div>
-      <p style={styles}>Hello {name}</p>
-      <p>The sume of {x} and {y} is {x + y}</p>
-      <ul>
-        {names.map((name, index) => (
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
-      {loggedIn && <h1>Hello User</h1>}
-    </div>
-    
+    <div>App</div>
   )
 }
 
